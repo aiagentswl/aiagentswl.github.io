8fe6a5340cc4afeb74f3309bd900211b426c20bb
aiagentswl <heritageartacademy44@gmail.com>
2026-09-01 17:16:14 +0600

    Add Firebase configuration to env.js

---
+++ env.js
@@ -0,0 +1,8 @@
+window.ENV_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCVDNtfeHmyeiLevNkWaAOIQs3dF1Yx6Rk",
+  authDomain: "http://kick-a9ee1.firebaseapp.com",
+  projectId: "kick-a9ee1",
+  storageBucket: "http://kick-a9ee1.firebasestorage.app",
+  messagingSenderId: "613337178500",
+  appId: "1:613337178500:web:015277a7b8eec0230b7dd1"
+};
